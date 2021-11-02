bd25584353750e5bfa265a0019feabf79f41984b
dongzhaoqi <dong18765203770@163.com>
2021-11-02 21:28:42 +0800

    feat: InsertController

--- beta-spring/src/main/java/com/dong/beta/strategy/controller/InsertController.java
+++ beta-spring/src/main/java/com/dong/beta/strategy/controller/InsertController.java
@@ -3,6 +3,8 @@ package com.dong.beta.strategy.controller;
 import com.dong.beta.controller.vo.ResponseModel;
 import com.dong.beta.strategy.enums.InsertTypeEnum;
 import com.dong.beta.strategy.service.InsertService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@Api(tags = "Insert strategy")
 @RestController
 @Slf4j
 public class InsertController {
@@ -19,6 +21,7 @@ public class InsertController {
     @Autowired
     InsertService insertService;
 
+    @ApiOperation("insert")
     @PostMapping("/insert")
     public ResponseModel<String> insert(@RequestParam(value = "type") List<InsertTypeEnum> type,
                                         @RequestParam(value = "date") String date){
