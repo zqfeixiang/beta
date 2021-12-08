fb82d6d2e81323a947669e1342c57de6a84e5c11
dongzhaoqi <dong18765203770@163.com>
2021-12-08 15:12:29 +0800

    [feat]: Add es config

---
+++ beta-spring/src/main/java/com/dong/beta/config/EsRestClient.java
@@ -0,0 +1,32 @@
+package com.dong.beta.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class EsRestClient {
+
+    @Value("${elasticsearch.cluster-nodes}")
+    private String address;
+
+    @Bean
+    public RestHighLevelClient RestClient(){
+        List<HttpHost> hostList = new ArrayList<>();
+        String[] addressArray = address.split(";");
+        Arrays.stream(addressArray).forEach(e -> {
+            String[] strArray  = e.split(":");
+            if(null != strArray && strArray.length == 2){
+                hostList.add(new HttpHost(strArray[0], Integer.valueOf(strArray[1]),"http"));
+            }
+        });
+        return  new RestHighLevelClient(RestClient.builder(hostList.toArray(new HttpHost[]{})));
+    }
+}
