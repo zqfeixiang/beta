7e1994b81b826b46bb35b2bc29d8ac61b32a36f5
dongzhaoqi <dong18765203770@163.com>
2022-09-22 11:03:47 +0800

    1. Modify RabbitConfig

--- beta-spring/src/main/java/com/dong/beta/config/RabbitConfig.java
+++ beta-spring/src/main/java/com/dong/beta/config/RabbitConfig.java
@@ -1,11 +1,15 @@
 package com.dong.beta.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,7 +33,18 @@ public class RabbitConfig {
 
     @Bean
     public RabbitTemplate rabbitTemplate() {
-        return new RabbitTemplate(connectionFactory());
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(this.connectionFactory());
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        return rabbitTemplate;
     }
 
+    /**
+     * 自定义消息转换器，默认是jdk的序列化转换器，我们自定义为json的
+     *
+     * @param objectMapper json序列化实现类
+     */
+    @Bean
+    public MessageConverter jsonMessageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
 }
